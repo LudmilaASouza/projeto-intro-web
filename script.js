4a1f7e5d97d4014eac17dcf147b224fa1c3594fb
Ludmila de Aguiar Souza <ludmilasouz.a@hotmail.com>
2022-10-07 19:18:51 -0300

    Envio Projeto Semana 4

--- script.js
+++ script.js
@@ -28,35 +28,47 @@ const musica3 = {
 }
 
 const musicas = [ ]
-musicas.push ( {
-    titulo1: "Um Dia Após o Outro", 
-    artista1: "Tiago Iorc",
-    noLancamento1: 2013,
-    genero1: ["Folk-pop", "Pop", "MPB", "Rock Alternativo"],
-    estrofe1: "Pra começar \n Cada coisa em seu lugar \n E nada como um dia após o outro",
-    autoral1: true
-    },
-    {
-    titulo2: "Pra Lá de Bagdá",
-    artista2: "Lagum",
-    anoLancamento2: "2016",
-    genero2: ["Pop", "Pop-rock", "Raggae"],
-    estrofe2: "Sempre me imaginei assim \n Livre pra fazer o que eu 'tiver a fim \n Vamos lá e seja o que eu quiser \n E o futuro só pertence à mim",
-    autoral2: false
-    },
-    {
-    titulo3: "Linda de Morrer",
-    artista3: "Marcello Gugu",
-    anoLancamento3: "2013",
-    genero3: ["Rap", "Hip-Hop"],
-    estrofe3: "Agradeça cada amanhecer, sorria mais \n Se preocupe menos \n Afinal o que tem solução se solucionará \n O que não tem, solucionado está",
-    autoral3: true
-    } )
 
-    console.log(musicas)
+if (musica1.autoral1 === true){
+    musicas.push ( {
+        titulo1: "Um Dia Após o Outro", 
+        artista1: "Tiago Iorc",
+        anoLancamento1: 2013,
+        genero1: ["Folk-pop", "Pop", "MPB", "Rock Alternativo"],
+        estrofe1: "Pra começar \n Cada coisa em seu lugar \n E nada como um dia após o outro",
+        autoral1: true 
+    })
+} else {
+    alert("A MÚSICA " + musica1.titulo1 + " NÃO É AUTORAL E NÃO PODE SER INCLUSA.")
+}
 
+if (musica2.autoral2 === true){
+    musicas.push ({
+        titulo2: "Pra Lá de Bagdá",
+        artista2: "Lagum",
+        anoLancamento2: "2016",
+        genero2: ["Pop", "Pop-rock", "Raggae"],
+        estrofe2: "Sempre me imaginei assim \n Livre pra fazer o que eu 'tiver a fim \n Vamos lá e seja o que eu quiser \n E o futuro só pertence à mim",
+        autoral2: false
+    })
+} else {
+    alert("A MÚSICA " + musica2.titulo2 + " NÃO É AUTORAL E NÃO PODE SER INCLUSA.")
+}
 
+if (musica3.autoral3 === true){
+    musicas.push ({
+        titulo3: "Linda de Morrer",
+        artista3: "Marcello Gugu",
+        anoLancamento3: "2013",
+        genero3: ["Rap", "Hip-Hop"],
+        estrofe3: "Agradeça cada amanhecer, sorria mais \n Se preocupe menos \n Afinal o que tem solução se solucionará \n O que não tem, solucionado está",
+        autoral3: true
+    })
+} else {
+    alert("A MÚSICA " + musica3.titulo3 + " NÃO É AUTORAL E NÃO PODE SER INCLUSA." )
+}
 
+console.log(musicas)
 
 
 /*const mediaLancento = Math.round((anoLancamento1+anoLancamento2+anoLancamento3)/3)
