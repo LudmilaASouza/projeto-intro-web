9f0cd7f413a11b538f769ea99a35170b7cf1b669
Ludmila de Aguiar Souza <ludmilasouz.a@hotmail.com>
2022-10-07 20:15:02 -0300

    Envio Projeto Semana 5

--- script.js
+++ script.js
@@ -1,74 +1,41 @@
 //MÚSICAS E SUAS HISTÓRIAS
 
-const musica1 = {
-    titulo1: "Um Dia Após o Outro",
-    artista1: "Tiago Iorc",
-    anoLancamento1: 2013,
-    genero1: ["Folk-pop", "Pop", "MPB", "Rock Alternativo"],
-    estrofe1: "Pra começar \n Cada coisa em seu lugar \n E nada como um dia após o outro",
-    autoral1: true
+let musicas = [
+    {
+    "titulo": "Um Dia Após o Outro", 
+    "artista": "Tiago Iorc",
+    "anoLancamento": 2013,
+    "album": "Zeski",
+    "genero": "Folk-pop, Pop, MPB, Rock Alternativo",
+    "estrofe": "Pra começar  Cada coisa em seu lugar E nada como um dia após o outro",
+    "autoral": true
+    },
+    {
+    "titulo": "Pra Lá de Bagdá",
+    "artista": "Lagum",
+    "anoLancamento": "2016",
+    "album": "Seja o Que Eu Quiser",
+    "genero": "Pop, Pop-rock, Raggae",
+    "estrofe": "Sempre me imaginei assim Livre pra fazer o que eu 'tiver a fim Vamos lá e seja o que eu quiser E o futuro só pertence à mim",
+    "autoral": false
+    },
+    {
+    "titulo": "Linda de Morrer",
+    "artista": "Marcello Gugu",
+    "anoLancamento": "2013",
+    "album": "Até Que Enfim Gugu",
+    "genero": "Rap, Hip-Hop",
+    "estrofe": "Agradeça cada amanhecer, sorria mais  Se preocupe menos Afinal o que tem solução se solucionará  O que não tem, solucionado está",
+    "autoral": true
+    } 
+]
+
+for (musicas of musicas){
+    for (itens in musicas){
+        console.log(`${itens}: ${musicas[itens]}`)
+    }
+console.log("------x------")
 }
-
-const musica2 = {
-    titulo2: "Pra Lá de Bagdá",
-    artista2: "Lagum",
-    anoLancamento2: "2016",
-    genero2: ["Pop", "Pop-rock", "Raggae"],
-    estrofe2: "Sempre me imaginei assim \n Livre pra fazer o que eu 'tiver a fim \n Vamos lá e seja o que eu quiser \n E o futuro só pertence à mim",
-    autoral2: false
-}
-
-const musica3 = {
-    titulo3: "Linda de Morrer",
-    artista3: "Marcello Gugu",
-    anoLancamento3: "2013",
-    genero3: ["Rap", "Hip-Hop"],
-    estrofe3: "Agradeça cada amanhecer, sorria mais \n Se preocupe menos \n Afinal o que tem solução se solucionará \n O que não tem, solucionado está",
-    autoral3: true
-}
-
-const musicas = [ ]
-
-if (musica1.autoral1 === true){
-    musicas.push ( {
-        titulo1: "Um Dia Após o Outro", 
-        artista1: "Tiago Iorc",
-        anoLancamento1: 2013,
-        genero1: ["Folk-pop", "Pop", "MPB", "Rock Alternativo"],
-        estrofe1: "Pra começar \n Cada coisa em seu lugar \n E nada como um dia após o outro",
-        autoral1: true 
-    })
-} else {
-    alert("A MÚSICA " + musica1.titulo1 + " NÃO É AUTORAL E NÃO PODE SER INCLUSA.")
-}
-
-if (musica2.autoral2 === true){
-    musicas.push ({
-        titulo2: "Pra Lá de Bagdá",
-        artista2: "Lagum",
-        anoLancamento2: "2016",
-        genero2: ["Pop", "Pop-rock", "Raggae"],
-        estrofe2: "Sempre me imaginei assim \n Livre pra fazer o que eu 'tiver a fim \n Vamos lá e seja o que eu quiser \n E o futuro só pertence à mim",
-        autoral2: false
-    })
-} else {
-    alert("A MÚSICA " + musica2.titulo2 + " NÃO É AUTORAL E NÃO PODE SER INCLUSA.")
-}
-
-if (musica3.autoral3 === true){
-    musicas.push ({
-        titulo3: "Linda de Morrer",
-        artista3: "Marcello Gugu",
-        anoLancamento3: "2013",
-        genero3: ["Rap", "Hip-Hop"],
-        estrofe3: "Agradeça cada amanhecer, sorria mais \n Se preocupe menos \n Afinal o que tem solução se solucionará \n O que não tem, solucionado está",
-        autoral3: true
-    })
-} else {
-    alert("A MÚSICA " + musica3.titulo3 + " NÃO É AUTORAL E NÃO PODE SER INCLUSA." )
-}
-
-console.log(musicas)
 
 
 /*const mediaLancento = Math.round((anoLancamento1+anoLancamento2+anoLancamento3)/3)
